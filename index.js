9aa8172243abe1df35aad26ac5705b5228ded266
reapedjuggler <tomarvibhav55@gmail.com>
2020-10-06 22:24:12 +0530

    added keypress events

--- index.js
+++ index.js
@@ -39,20 +39,76 @@ function ace() {
      
 }
 
-// function TempMan(name , a ge , hobbies){
+// function TempMan(name ,  age , hobbies){
 
 //       this.name = name;
 //       this.age = age;
 //       this.hobbies = hobbies;     
+//       this.me = function(){
+//         alert("you will mofo have patience");
+//       };
 // }   
 
 // var luffy1 = new TempMan("luffy" , 12 , ["tt" , "problem solving"]);
 
 // console.log(luffy1);
 
-for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click" , ace);
+function sound(temp) {
+
+    
+    // console.log("hello"); 
+    // console.log(this);
+    
+     switch (temp) {
+         case "w":
+             var audio = new Audio("sounds/crash.mp3");
+             audio.play();
+             break; 
+         case "a":
+             var audio = new Audio("sounds/kick-bass.mp3");
+             audio.play();
+             break;
+         case "s":
+             var audio = new Audio("sounds/snare.mp3");
+             audio.play();
+             break;
+         case "d":
+             var audio = new Audio("sounds/tom-1.mp3");
+             audio.play();
+             break;
+         case "j":
+             var audio = new Audio("sounds/tom-2.mp3");
+             audio.play();
+             break;
+         case "k":
+             var audio = new Audio("sounds/tom-3.mp3");
+             audio.play();
+             break;
+         case "l":
+             var audio = new Audio("sounds/tom-4.mp3");
+             audio.play();
+             break;
+         default:
+             break;
+     }
+     
 }
+
+
+
+for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
+    // console.log("rocket science");   
+    var buttenHtml = this.innerHTML;
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+        sound(this.innerHTML);
+    });
+}   
+
+document.addEventListener("keypress" , function(event){
+
+    sound(event.key);
+
+});
 
 
 
